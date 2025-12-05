b0ad08aa66347edb581ad2c57a23a6aa813873b2
ARTHUR TEIXEIRA PERILLO <109046338+ArthurTPerillo@users.noreply.github.com>
2025-12-05 14:51:27 -0300

    Refactor run_tests.sh for syntactic and semantic tests

--- run_tests.sh
+++ run_tests.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 
-TEST_DIR="testes"
+TEST_DIR1="testes_sintatico"
+TEST_DIR2="testes_semantico"
 
 EXEC="./goianinha"
 
-mkdir -p outputs
+mkdir -p outputs_sintatico
+mkdir -p outputs_semantico
 
-for file in "$TEST_DIR"/*.g; do
+for file in "$TEST_DIR1"/*.g; do
     
     base=$(basename "$file" .g)
     echo "Rodando teste: $file"
-    "$EXEC" "$file" > "outputs/$base.out" 2>&1
+    "$EXEC" "$file" > "outputs_sintatico/$base.out" 2>&1
 done
 
-echo "✔ Testes concluídos! Veja os arquivos em outputs/"
+for file in "$TEST_DIR2"/*.g; do
+    
+    base=$(basename "$file" .g)
+    echo "Rodando teste: $file"
+    "$EXEC" "$file" > "outputs_semantico/$base.out" 2>&1
+done
+
+echo "Testes concluídos! Veja os arquivos em outputs_sintatico e outputs_semantico/"
